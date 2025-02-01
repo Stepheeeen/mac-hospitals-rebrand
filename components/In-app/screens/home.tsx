a6211ef2e0aaf886dafc59be9736e4e4f9f0b213
stepheeeen <stephenon56@gmail.com>
2025-02-02 00:53:32 +0100

    refactor: update user state in Home component and comment out unused BottomNav code

--- components/In-app/screens/home.tsx
+++ components/In-app/screens/home.tsx
@@ -3,12 +3,12 @@ import { ScrollView, View, Image, Text } from "react-native";
 import tw from "twrnc";
 
 const Home = () => {
-  const [user] = useState("");
+  const [user] = useState("David");
   const [imageUrl] = useState("");
   return (
     <>
       <View style={tw`flex-row justify-between`}>
-        <Image source={require("../../assets/images/twemoji_ambulance.png")} />
+        {/* <Image source={require("../../assets/images/twemoji_ambulance.png")} /> */}
 
         {/* i honestly dont know what i did here but you should understand what i'm doing Sir */}
         <Text>Hello, {user}</Text>
